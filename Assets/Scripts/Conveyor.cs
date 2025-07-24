54f6255a87e64f157a628c9821a945a29cfe1f47
Мусин Михаил <musin.mihail@gmail.com>
2025-07-24 23:52:32 +0300

    Рефакторинг

--- Assets/Scripts/Conveyor.cs
+++ Assets/Scripts/Conveyor.cs
@@ -1,22 +1,13 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 /// <summary>
 /// Управляет состоянием, типом и ориентацией одного конвейера.
+/// Логика была переработана для использования битовой маски, что упрощает определение внешнего вида.
 /// </summary>
 [RequireComponent(typeof(SpriteRenderer))]
 public class Conveyor : MonoBehaviour
 {
-    /// <summary>
-    /// Перечисление возможных направлений для прямого конвейера.
-    /// </summary>
-    public enum Direction
-    {
-        None,
-        Up,
-        Down,
-        Left,
-        Right
-    }
     [Header("Sprites")]
     [Tooltip("Спрайт для прямого конвейера.")]
     public Sprite straightSprite;
@@ -28,81 +19,91 @@ public class Conveyor : MonoBehaviour
     public Sprite crossSprite;
     private SpriteRenderer _spriteRenderer;
 
+    /// <summary>
+    /// Словарь, который сопоставляет битовую маску соединений с соответствующим типом конвейера (спрайт и поворот).
+    /// </summary>
+    private Dictionary<int, ConveyorType> _conveyorTypes;
+
+    /// <summary>
+    /// Структура для хранения данных о типе конвейера: спрайт и угол поворота.
+    /// </summary>
+    private struct ConveyorType
+    {
+        public readonly Sprite Sprite;
+        public readonly float Rotation;
+
+        public ConveyorType(Sprite sprite, float rotation)
+        {
+            Sprite = sprite;
+            Rotation = rotation;
+        }
+    }
+
     private void Awake()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
+        InitializeConveyorTypes();
     }
 
     /// <summary>
-    /// Устанавливает тип конвейера (прямой) и поворачивает его в соответствии с направлением.
+    /// Инициализирует словарь, заполняя его всеми возможными комбинациями соединений (масками)
+    /// и соответствующими им спрайтами и углами поворота.
     /// </summary>
-    /// <param name="direction">Новое направление.</param>
-    public void SetStraight(Direction direction)
+    private void InitializeConveyorTypes()
     {
-        if (_spriteRenderer.sprite != straightSprite)
+        _conveyorTypes = new Dictionary<int, ConveyorType>
+        {
+            // Маска 0: Нет соединений (изолированный конвейер)
+            [0] = new(straightSprite, 0),
+            // Маска 1: Только вверх
+            [1] = new(straightSprite, 0),
+            // Маска 2: Только вниз
+            [2] = new(straightSprite, 180),
+            // Маска 3: Вверх и Вниз (прямой вертикальный)
+            [3] = new(straightSprite, 0),
+            // Маска 4: Только влево
+            [4] = new(straightSprite, 90),
+            // Маска 5: Вверх и Влево (угол)
+            [5] = new(cornerSprite, 90),
+            // Маска 6: Вниз и Влево (угол)
+            [6] = new(cornerSprite, 180),
+            // Маска 7: Вверх, Вниз, Влево (Т-образный)
+            [7] = new(tJunctionSprite, 90),
+            // Маска 8: Только вправо
+            [8] = new(straightSprite, -90),
+            // Маска 9: Вверх и Вправо (угол)
+            [9] = new(cornerSprite, 0),
+            // Маска 10: Вниз и Вправо (угол)
+            [10] = new(cornerSprite, -90),
+            // Маска 11: Вверх, Вниз, Вправо (Т-образный)
+            [11] = new(tJunctionSprite, -90),
+            // Маска 12: Влево и Вправо (прямой горизонтальный)
+            [12] = new(straightSprite, 90),
+            // Маска 13: Вверх, Влево, Вправо (Т-образный)
+            [13] = new(tJunctionSprite, 0),
+            // Маска 14: Вниз, Влево, Вправо (Т-образный)
+            [14] = new(tJunctionSprite, 180),
+            // Маска 15: Все 4 направления (крестовина)
+            [15] = new(crossSprite, 0)
+        };
+    }
+
+    /// <summary>
+    /// Обновляет внешний вид конвейера на основе маски его соединений.
+    /// </summary>
+    /// <param name="connectionMask">Битовуя маска, представляющая соединения с соседями.</param>
+    public void UpdateState(int connectionMask)
+    {
+        if (_conveyorTypes.TryGetValue(connectionMask, out var type))
+        {
+            _spriteRenderer.sprite = type.Sprite;
+            transform.rotation = Quaternion.Euler(0, 0, type.Rotation);
+        }
+        else
         {
             _spriteRenderer.sprite = straightSprite;
+            transform.rotation = Quaternion.identity;
+            Debug.LogWarning($"Не найдена конфигурация для маски соединений: {connectionMask}");
         }
-
-        transform.rotation = Quaternion.Euler(0, 0, GetRotationForDirection(direction));
-    }
-
-    /// <summary>
-    /// Устанавливает тип конвейера (угловой) и поворачивает его на заданный угол.
-    /// </summary>
-    /// <param name="angle">Угол поворота в градусах.</param>
-    public void SetCorner(float angle)
-    {
-        if (_spriteRenderer.sprite != cornerSprite)
-        {
-            _spriteRenderer.sprite = cornerSprite;
-        }
-
-        transform.rotation = Quaternion.Euler(0, 0, angle);
-    }
-
-    /// <summary>
-    /// Устанавливает тип конвейера (Т-образный) и поворачивает его на заданный угол.
-    /// </summary>
-    /// <param name="angle">Угол поворота в градусах.</param>
-    public void SetTJunction(float angle)
-    {
-        if (_spriteRenderer.sprite != tJunctionSprite)
-        {
-            _spriteRenderer.sprite = tJunctionSprite;
-        }
-
-        transform.rotation = Quaternion.Euler(0, 0, angle);
-    }
-
-    /// <summary>
-    /// Устанавливает тип конвейера (крестовой).
-    /// </summary>
-    public void SetCross()
-    {
-        if (_spriteRenderer.sprite != crossSprite)
-        {
-            _spriteRenderer.sprite = crossSprite;
-        }
-
-        transform.rotation = Quaternion.identity;
-    }
-
-
-    /// <summary>
-    /// Возвращает угол поворота в градусах для указанного направления (для прямых конвейеров).
-    /// </summary>
-    /// <param name="direction">Направление.</param>
-    /// <returns>Угол в градусах.</returns>
-    private static float GetRotationForDirection(Direction direction)
-    {
-        return direction switch
-        {
-            Direction.Up => 0,
-            Direction.Right => -90,
-            Direction.Down => 180,
-            Direction.Left => 90,
-            _ => 0
-        };
     }
 }
--- Assets/Scripts/GridInteraction.cs
+++ Assets/Scripts/GridInteraction.cs
@@ -1,11 +1,11 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 /// <summary>
 /// Обрабатывает взаимодействие игрока с сеткой, например, размещение объектов.
 /// Взаимодействует с GridGenerator для получения информации о ячейках.
+/// Логика обновления конвейеров была переработана для использования битовых масок.
 /// </summary>
 public class GridInteraction : MonoBehaviour
 {
@@ -110,10 +110,7 @@ public class GridInteraction : MonoBehaviour
     /// <param name="centerCell">Ячейка, вокруг которой нужно произвести обновление.</param>
     private void UpdateSurroundingConveyors(Cell centerCell)
     {
-        if (centerCell.IsOccupied)
-        {
-            UpdateSingleConveyorOrientation(centerCell);
-        }
+        UpdateSingleConveyorOrientation(centerCell);
 
         foreach (var neighborCell in _gridGenerator.GetNeighbors(centerCell).Where(neighborCell => neighborCell.IsOccupied))
         {
@@ -122,115 +119,40 @@ public class GridInteraction : MonoBehaviour
     }
 
     /// <summary>
-    /// Определяет и устанавливает правильную ориентацию для одного конвейера
-    /// на основе его текущих соседей.
+    /// Вычисляет битовую маску соединений для конвейера в указанной ячейке
+    /// и передает ее в компонент Conveyor для обновления его внешнего вида.
     /// </summary>
     /// <param name="cell">Ячейка с конвейером для обновления.</param>
     private void UpdateSingleConveyorOrientation(Cell cell)
     {
         var conveyor = cell.GetConveyor();
         if (!conveyor) return;
-
-        var neighborConveyors = _gridGenerator.GetNeighbors(cell)
-            .Where(c => c.IsOccupied && c.GetConveyor())
-            .ToList();
-
-        switch (neighborConveyors.Count)
+        var gridPos = cell.GridPosition;
+        var connectionMask = 0;
+        var neighborUp = _gridGenerator.GetCell(gridPos + Vector2Int.up);
+        if (neighborUp is { IsOccupied: true })
         {
-            case 0:
-                conveyor.SetStraight(Conveyor.Direction.Up);
-                break;
-            case 1:
-            {
-                var directionVector = neighborConveyors[0].GridPosition - cell.GridPosition;
-                conveyor.SetStraight(GetDirectionFromVector(directionVector));
-                break;
-            }
-            case 2:
-            {
-                var dirToFirst = neighborConveyors[0].GridPosition - cell.GridPosition;
-                var dirToSecond = neighborConveyors[1].GridPosition - cell.GridPosition;
-
-                if (dirToFirst == -dirToSecond)
-                {
-                    conveyor.SetStraight(GetDirectionFromVector(dirToFirst));
-                }
-                else
-                {
-                    conveyor.SetCorner(GetAngleForCorner(dirToFirst, dirToSecond));
-                }
-
-                break;
-            }
-            case 3:
-            {
-                var angle = GetAngleForTJunction(neighborConveyors, cell);
-                conveyor.SetTJunction(angle);
-                break;
-            }
-            case 4:
-            {
-                conveyor.SetCross();
-                break;
-            }
-            default:
-                conveyor.SetStraight(Conveyor.Direction.Up);
-                break;
+            connectionMask |= 1;
         }
-    }
 
-    /// <summary>
-    /// Вычисляет угол для Т-образного конвейера.
-    /// Находит единственное направление, где нет соседа, и поворачивает "открытую" часть конвейера туда.
-    /// </summary>
-    /// <param name="neighbors">Список из трех соседних ячеек с конвейерами.</param>
-    /// <param name="center">Центральная ячейка, для которой вычисляется поворот.</param>
-    /// <returns>Угол поворота в градусах.</returns>
-    private float GetAngleForTJunction(List<Cell> neighbors, Cell center)
-    {
-        var neighborDirections = neighbors.Select(n => n.GridPosition - center.GridPosition).ToList();
-
-        var allDirections = new List<Vector2Int>
+        var neighborDown = _gridGenerator.GetCell(gridPos + Vector2Int.down);
+        if (neighborDown is { IsOccupied: true })
         {
-            Vector2Int.up, Vector2Int.down, Vector2Int.left, Vector2Int.right
-        };
+            connectionMask |= 2;
+        }
 
-        var missingDirection = allDirections.FirstOrDefault(dir => !neighborDirections.Contains(dir));
+        var neighborLeft = _gridGenerator.GetCell(gridPos + Vector2Int.left);
+        if (neighborLeft is { IsOccupied: true })
+        {
+            connectionMask |= 4;
+        }
 
-        if (missingDirection == Vector2Int.up) return 180f;
-        if (missingDirection == Vector2Int.down) return 0f;
-        if (missingDirection == Vector2Int.left) return -90f;
-        if (missingDirection == Vector2Int.right) return 90f;
+        var neighborRight = _gridGenerator.GetCell(gridPos + Vector2Int.right);
+        if (neighborRight is { IsOccupied: true })
+        {
+            connectionMask |= 8;
+        }
 
-        return 0f;
-    }
-
-    /// <summary>
-    /// Вычисляет угол поворота для углового конвейера на основе векторов к его двум соседям.
-    /// </summary>
-    /// <param name="dir1">Вектор направления к первому соседу.</param>
-    /// <param name="dir2">Вектор направления ко второму соседу.</param>
-    /// <returns>Угол в градусах для поворота спрайта.</returns>
-    private float GetAngleForCorner(Vector2Int dir1, Vector2Int dir2)
-    {
-        var cornerDirection = dir1 + dir2;
-        if (cornerDirection == new Vector2Int(1, 1)) return 0;
-        if (cornerDirection == new Vector2Int(1, -1)) return -90;
-        if (cornerDirection == new Vector2Int(-1, -1)) return 180;
-        if (cornerDirection == new Vector2Int(-1, 1)) return 90;
-        Debug.LogWarning($"Не удалось определить угол для направлений {dir1} и {dir2}");
-        return 0;
-    }
-
-    /// <summary>
-    /// Преобразует вектор направления в перечисление Conveyor.Direction.
-    /// </summary>
-    private Conveyor.Direction GetDirectionFromVector(Vector2Int vector)
-    {
-        if (vector == Vector2Int.up) return Conveyor.Direction.Up;
-        if (vector == Vector2Int.down) return Conveyor.Direction.Down;
-        if (vector == Vector2Int.left) return Conveyor.Direction.Left;
-        if (vector == Vector2Int.right) return Conveyor.Direction.Right;
-        return Conveyor.Direction.None;
+        conveyor.UpdateState(connectionMask);
     }
 }
